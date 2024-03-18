f24ccc72a3fb74a6977c24d781096c670ec31e9e
Shu-Jeng Hsieh <shu-jenghsieh@Shu-Jengs-MacBook-Pro.local>
2024-03-18 22:37:15 +0800

    deploy required buckets

--- bin/real-time-streaming-kds.ts
+++ bin/real-time-streaming-kds.ts
@@ -15,7 +15,7 @@ new RealTimeStreamingKdsStack(app, 'RealTimeStreamingKdsStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: cdk.Aws.ACCOUNT_ID, region: cdk.Aws.REGION },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
