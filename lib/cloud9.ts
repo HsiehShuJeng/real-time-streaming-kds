6368991de04cda6a6fb0ba5218438ce8f3d9600a
Shu-Jeng Hsieh <shu-jenghsieh@Shu-Jengs-MacBook-Pro.local>
2024-03-26 23:03:31 +0800

    update cloud9 instance in cdk v2 using typescript 1. Removed AWS::Cloud9::EnvironmentEC2 properties block for Cloud9 instance. 2. Updated Cloud9Instance class to align with AWS CDK v2 specifications using Typescript. 3. Ensured compatibility of Cloud9 instance configuration with the new CDK version.

--- lib/cloud9.ts
+++ lib/cloud9.ts
@@ -2,17 +2,6 @@ import * as cdk from 'aws-cdk-lib';
 import * as cloud9 from 'aws-cdk-lib/aws-cloud9';
 import { Construct } from 'constructs';
 
-/*
- * Cloud9:
-    Type: AWS::Cloud9::EnvironmentEC2
-    Properties:
-      AutomaticStopTimeMinutes: 30
-      Description: Real Time Streaming with Amazon Kinesis Cloud9 IDE 
-      InstanceType: m5.large
-      Name: !Sub "KinesisRealTimeStreaming-${AWS::StackName}"
-      ImageId: amazonlinux-2-x86_64
-    I need a cdk v2 version of the above resource using Typescript
-*/
 export class Cloud9Instance extends Construct {
     public readonly entity: cloud9.CfnEnvironmentEC2;
     constructor(scope: Construct, id: string){
---
+++ lib/search-engine.ts
@@ -0,0 +1,68 @@
+import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as opensearch from 'aws-cdk-lib/aws-opensearchservice';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
+import { Construct } from 'constructs';
+
+interface OpenSearchProps {
+    domainName?: string
+}
+
+export class OpenSearch extends Construct {
+    public readonly entity: opensearch.Domain;
+    constructor(scope: Construct, id: string, props?: OpenSearchProps){
+        super(scope, id)
+        const pwdSecret = new secretsmanager.Secret(this, 'Password', {
+            generateSecretString: {
+                secretStringTemplate: JSON.stringify({
+                    username: 'admin'
+                }),
+                generateStringKey: 'password',
+                passwordLength: 16,
+                excludeCharacters: "\"@/\\"
+            }
+        });
+        const domainName = props?.domainName ?? 'os-domain'
+        this.entity = new opensearch.Domain(this, 'Domain', {
+            domainName: domainName,
+            version: opensearch.EngineVersion.OPENSEARCH_1_2,
+            capacity: {
+                dataNodes: 1,
+                dataNodeInstanceType: 't3.medium.search'
+            },
+            accessPolicies: [
+                new iam.PolicyStatement({
+                    effect: iam.Effect.ALLOW,
+                    principals: [new iam.AnyPrincipal()],
+                    actions: ['es:*'],
+                    resources: [`arn:aws:es:${cdk.Aws.REGION}:${cdk.Aws.ACCOUNT_ID}:domain/${domainName}/*`]
+                })
+            ],
+            fineGrainedAccessControl: {
+                masterUserName: pwdSecret.secretValueFromJson('username').toString(),
+                masterUserPassword: pwdSecret.secretValueFromJson('password')
+            },
+            nodeToNodeEncryption: true,
+            encryptionAtRest: {
+                enabled: true
+            },
+            enforceHttps: true,
+            ebs: {
+                volumeSize: 10,
+                volumeType: ec2.EbsDeviceVolumeType.GP2
+            },
+        });
+        new cdk.CfnOutput(this, 'PasswordSecret', {
+            value: pwdSecret.secretArn,
+            description: 'The ARN of the OpenSearch password secret.'
+        });
+        new cdk.CfnOutput(this, 'DomainEndpointArn', {
+            value: this.entity.domainArn, 
+            description: 'The ARN of the OpenSearch domain.' });
+        new cdk.CfnOutput(this, 'DomainNameEndpoint', {
+            value: this.entity.domainEndpoint,
+            description: 'The endpoint of the OpenSearch domain.'
+        });
+    }
+}
