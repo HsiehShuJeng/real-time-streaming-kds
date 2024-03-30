a9876ec26c407c48899d8332ca09cf97f448ce37
Shu-Jeng Hsieh <shu-jenghsieh@Shu-Jengs-MBP.lan>
2024-03-30 18:46:03 +0800

    clarify the default value for cloud9 instance owner 1. Update the log message in the `Cloud9Instance` class to clearly indicate that the `ownerArn` property is being used as the default value if it is not specified. 2. Remove the unnecessary commented-out code for the Cloud9 environment URL, as it is already being output using the `CfnOutput`.

--- lib/cloud9.ts
+++ lib/cloud9.ts
@@ -11,7 +11,7 @@ export class Cloud9Instance extends Construct {
         super(scope, id)
         const ownerArn = props?.ownerArn ?? `arn:aws:iam::${cdk.Aws.ACCOUNT_ID}:user/Administrator`;
         if (!props?.ownerArn){
-            console.log(`ownerArn is not specificed. The default content of ownerArn will be ${ownerArn}.`)
+            console.log(`ownerArn as property for Cloud9Instance is not specificed. The default value, i.e., ${ownerArn} will be assigned to ownerArn.`)
         }
         this.entity = new cloud9.CfnEnvironmentEC2(this, 'Cloud9Instance', {
             automaticStopTimeMinutes: 30,
@@ -21,7 +21,6 @@ export class Cloud9Instance extends Construct {
             imageId: 'amazonlinux-2-x86_64',
             ownerArn: ownerArn
         })
-        // https://${AWS::Region}.console.aws.amazon.com/cloud9/ide/
         new cdk.CfnOutput(this, 'Cloud9Url', {value: `https://${cdk.Aws.REGION}.console.aws.amazon.com/cloud9/ide/${this.entity.ref}`, description: 'The Cloud9 environment.'})
     }
 }
